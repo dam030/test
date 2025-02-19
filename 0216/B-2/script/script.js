1907d1106e0848147a258d85fd346651a23f0b43
dam030 <yun99hj@gmail.com>
2025-02-19 18:45:36 +0900

    0219

--- 0216/B-2/script/script.js
+++ 0216/B-2/script/script.js
@@ -1,6 +1,22 @@
 $(function(){
 // 기본 세팅
-    $('.navbar>ul>li').mouseenter(function(){
-        $(this).find('.sub').stop().slideDown()
+    $('.navbar>ul').mouseenter(function(){
+        $('.sub').slideDown();
+        $('.sub_bg').slideDown();
     });
+    $('.navbar>ul').mouseleave(function(){
+        $('.sub').slideUp();
+        $('.sub_bg').slideUp();
+    });
+    // navbar 세팅
+
+    // 슬라이드 세팅
+
+    $('.modal').click(function(){
+        $('.popup_bg').show();
+    });
+    $('.popup_bg>,popup>a').click(function(){
+        $('.popup_bg').hide();
+    });
+    // 모달창 세팅
 });
