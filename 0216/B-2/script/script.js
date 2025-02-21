20ef0af6f7cab2f553e6919957305d19355fbc59
dam030 <yun99hj@gmail.com>
2025-02-21 18:45:55 +0900

    0221

--- 0216/B-2/script/script.js
+++ 0216/B-2/script/script.js
@@ -10,6 +10,13 @@ $(function(){
     });
     // navbar 세팅
 
+    function slideMove(){
+        $('.slides').animate({'left':'-1200px'}, 500, function(){
+            $('.slides>a:first-child').appendTo($('.slides'));
+            $('.slides').css('left','0px');
+        });
+    };
+    setInterval(slideMove, 3000);
     // 슬라이드 세팅
 
     $('.modal').click(function(){
