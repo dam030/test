0efc071b9e2dcabf44369fb169f43d5f26b95135
dam030 <yun99hj@gmail.com>
2025-02-25 18:35:04 +0900

    0225

--- 0224/d-1/scrip/script.js
+++ 0224/d-1/scrip/script.js
@@ -1,3 +1,28 @@
 $(function(){
     // 기본 세팅
+    $('nav>ul>li').mouseenter(function(){
+        // $('.sub').slideUp();
+        $(this).find('.sub').slideDown();
+    });
+    $('nav>ul>li').mouseleave(function(){
+        $(this).find('.sub').slideUp();
+    });
+    // 나브바
+
+    function slideMove(){
+        $('.slide').animate({'top':'400px'}, 500, function(){
+            $('.slide>li:first-child').appendTo($('.slide'));
+            $('.slide').css('top', '0px')
+        });
+    };
+    setInterval(slideMove, 3000);
+    // 슬라이드
+
+    $('.modal').click(function(){
+        $('.popup').show();
+    });
+    $('.close').click(function(){
+        $('.popup').hide();
+    });
+    // 모달
 });
