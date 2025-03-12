5d83d9519772dc56e84376b4199a29ab4ddb39ec
dam030 <yun99hj@gmail.com>
2025-03-12 21:54:27 +0900

    0312

--- 0312/20250312_윤혜지/script/script.js
+++ 0312/20250312_윤혜지/script/script.js
@@ -1,3 +1,25 @@
 $(function(){
     // 기본 세팅
+    $('.navbar>ul>li').mouseover(function(){
+        $(this).find('.sub').slideDown();
+    });
+    $('.navbar>ul>li').mouseleave(function(){
+        $(this).find('.sub').slideUp();
+    });
+
+    $('.modal').click(function(){
+        $('.popup_bg').show();
+    });
+    $('.close').click(function(){
+        $('.popup_bg').hide();
+    });
+
+    function slideMove(){
+        let slideWidth = $('.slide img').width();
+        $('.slide>ul').animate({'left':-slideWidth}, 300, function(){
+            $('.slide>ul>li').eq(0).appendTo($('.slide>ul'));
+            $('.slide>ul').css('left', '0px');
+        });
+    }
+    setInterval(slideMove, 3000);
 });
