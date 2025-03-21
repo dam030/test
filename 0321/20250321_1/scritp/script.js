102af0ce2e33313cfeeb608a0eeb86a2ad4dc42c
dam030 <yun99hj@gmail.com>
2025-03-21 17:47:42 +0900

    0321

--- 0321/20250321_1/scritp/script.js
+++ 0321/20250321_1/scritp/script.js
@@ -1,3 +1,36 @@
 $(function(){
     // 기본세팅
+    function slideMove(){
+        let slideHeight = $('.slide img').height();
+
+        $('.slide').animate({'top':-slideHeight}, 300, function(){
+            $('.slide>li').eq(0).appendTo($('.slide'));
+            $('.slide').css('top','0px')
+        });
+    };
+    setInterval(slideMove,3000)
+
+
+    $('.navbar>ul>li').mouseenter(function(event){
+        event.preventDefault()
+        $(this).find('.sub').slideDown();
+    });
+    $('.navbar>ul>li').mouseleave(function(event){
+        event.preventDefault()
+        $(this).find('.sub').slideUp();
+    });
+
+
+    $('.modal').click(function(){
+        $('.popup').show();
+    });
+    $('.close').click(function(){
+        $('.popup').hide();
+    });
+
+
+    $('.tab>a').click(function(){
+        $('.tab').removeClass('active');
+        $(this).parent().addClass('active');
+    });
 });
